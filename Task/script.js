0928454444923c616b933d06c0fffda7fa815fed
IbrahimAbilov <ibrahimeab@code.edu.az>
2024-03-13 20:37:46 +0400

    task

---
+++ Task/script.js
@@ -0,0 +1,26 @@
+let firstName = 'ibrahim'
+let lastName = 'Abilov'
+const dateOfBirth = '09.03.2002'
+let uni = false 
+let work =  false
+let age = 22
+let num1 = 7
+let num2 = '88'
+if (firstName === lastName || dateOfBirth !== 2001){
+    console.log("THIS IS AN ERROR YOU ARE ROBOT");
+}else{console.log(QƏBUL EDİLDİNİZ);
+}
+
+console.log(age+num1+num2);
+
+// adinizi kicik herflerle yazin 
+let userName = firstName.toLowerCase()
+console.log(firstName);
+
+// adinizi boyuk herflerle yazin
+let name = firstName.toUpperCase()
+console.log(name);
+
+// adin uzunlugu 
+let name2 = firstName.length + lastName.length
+console.log(name2);j
